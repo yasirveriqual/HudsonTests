ca82cfa7e73e5770c947c869a44f882672c40866
Yasir Ali <yasiraliraj@gmail.com>
2012-05-18 18:03:42 +0500

    Initial Commit

---
+++ HudsonTests/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HudsonTests
+//
+//  Created by Yasir Ali on 5/18/12.
+//  Copyright (c) 2012 VeriQual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HudsonTests/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HudsonTests
+//
+//  Created by Yasir Ali on 5/18/12.
+//  Copyright (c) 2012 VeriQual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
